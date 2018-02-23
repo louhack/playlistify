3398bc98c48e2cef84b0fb4082f545eaa027e323
louhack <loic.haquin@gmail.com>
2018-02-23 17:55:10 +0100

    change req limit

--- server/controllers/albums.controller.js
+++ server/controllers/albums.controller.js
@@ -9,24 +9,24 @@ _this = this;
 exports.getAlbums = async function(req, res, next){
 
     // Check the existence of the query parameters, If the exists doesn't exists assign a default value
-    
+
     var page = req.query.page ? req.query.page : 1
-    var limit = req.query.limit ? req.query.limit : 20; 
+    var limit = req.query.limit ? req.query.limit : 500;
 
     try{
-    
+
         var albums = await AlbumService.getAlbumsService({}, page, limit)
-        
+
         // Return the albums list with the appropriate HTTP Status Code and Message.
-        
+
         return res.status(200).json({status: 200, data: albums, message: "Albums Successfully Recieved"});
-        
+
     }catch(e){
-        
+
         //Return an Error Response Message with Code and the Error Message.
-        
+
         return res.status(400).json({status: 400, message: e.message});
-        
+
     }
 }
 
@@ -44,15 +44,15 @@ exports.createAlbum = async function(req, res, next){
     var album = req.body.album;
 
     try{
-        
+
         // Calling the Service function with the new object from the Request Body
-    
+
         var createdAlbum = await AlbumService.createAlbumService(album)
         return res.status(201).json({status: 201, data: createdAlbum, message: "Album Succesfully Created"})
     }catch(e){
-        
+
         //Return an Error Response Message with Code and the Error Message.
-        
+
         return res.status(400).json({status: 400, message: "Album Creation was Unsuccesfull"})
     }
 }
@@ -74,11 +74,11 @@ exports.updateAlbum = async function(req, res, next){
         artistName: req.body.artistName ? req.body.artistName : null,
         albumName: req.body.albumName ? req.body.albumName : null,
         idAlbumSputnik: req.body.idAlbumSputnik ? req.body.idAlbumSputnik : null,
-        imagePath: req.body.imagePath ? req.body.imagePath : null, 
-        note: req.body.note ? req.body.note : null, 
-        releaseMonth: req.body.releaseMonth ? req.body.releaseMonth : null, 
+        imagePath: req.body.imagePath ? req.body.imagePath : null,
+        note: req.body.note ? req.body.note : null,
+        releaseMonth: req.body.releaseMonth ? req.body.releaseMonth : null,
         spotifyId: req.body.spotifyId ? req.body.spotifyId : null,
-        
+
     }
 
 
