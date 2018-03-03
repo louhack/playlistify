33cb5d375fdacd98218d03df15f8dde2dcc7a76b
louhack <loic.haquin@gmail.com>
2018-03-03 11:39:36 +0100

    update script sputnik python

--- scripts/script_test_beautifulsoup_LXML_JSON_v2.py
+++ scripts/script_test_beautifulsoup_LXML_JSON_v2.py
@@ -6,6 +6,20 @@ import re
 from  pymongo import MongoClient
 from bson import json_util
 
+monthDic = {}
+monthDic["January"]=1
+monthDic["February"]=2
+monthDic["March"]=3
+monthDic["April"]=4
+monthDic["May"]=5
+monthDic["June"]=6
+monthDic["July"]=7
+monthDic["August"]=8
+monthDic["September"]=9
+monthDic["October"]=10
+monthDic["November"]=11
+monthDic["December"]=12
+
 r = requests.get("http://www.sputnikmusic.com/newreleases.php")
 if r.status_code != 200:
 	exit
@@ -18,18 +32,12 @@ soup = BeautifulSoup(r.text, 'lxml')
 #print(soup)
 
 #Je recupere le 1er mois affiche sur la page qui correspondant au mois en cours
-currentmonth = soup.find(class_="plaincontentbox").table.td.string
-# print("Voici les releases du mois de", lastmonth.string)
+#print(soup.find(class_="plaincontentbox").tr.next_sibling.td.string)
+currentmonth = soup.find(class_="plaincontentbox").tr.next_sibling.td.string
 
 
 #Je recupere le tableau des release du mois en cours
 table_releases = soup.find_all(class_="alt1")
-# print("Nombre element dans table_releases", len(table_releases), repr(table_releases))
-# print("Nombre element dans table_releases", len(table_releases))
-
-# with open('releases.csv', 'w', newline='') as f:
-# f=open('releases.csv', 'w', newline='', encoding='utf-8')
-# writer = csv.writer(f,delimiter=';')
 
 releases_list = []
 
@@ -38,28 +46,15 @@ i=j=l=1
 for table in table_releases:
 	if l==1:
 		mois_lu=currentmonth
+		releaseDate = mois_lu.split()
 		l=2
 	else:
 		mois_lu = table.parent.parent.previous_sibling.td.string
-		# print("table.parent:", table.parent.parent.previous_sibling.td)
-		# print("table.parent2:", table.parent.parent.next_sibling.td)
-		# print ("le tableau en cours", table)
-	for release in table:
+		releaseDate = mois_lu.split()
 
-		#Une ligne, 2 releases.
-			# print("Ligne :", i)
-			# print("============================ Album #", j, "============================")
-			#print("Contenu", release)
-			#print("release.span :", release.span)
+	for release in table:
 			note_release_1 = release.span.string
 			album_link_1 = release.find("a").get('href')
-			# artiste = row.td.next_sibling.string
-			# print("Artiste", row.td.next_sibling)
-
-
-			# artiste_album = row.td.next_sibling.stripped_strings
-			#print("Artiste et Album", repr(release.td.next_sibling))
-			# print ("Album", album[1])
 
 			k=0
 			for string in release.td.next_sibling.stripped_strings:
@@ -71,30 +66,9 @@ for table in table_releases:
 			 		k=0
 			 		# print("Artiste ou Album", string)
 
-			# print("Artiste :", artiste_1)
-			# print ("Album :", album_1)
-			# print("Note de l'album: :" , note_release_1)
-			# print("Lien Album :", album_link_1)
-			#print("http://www.sputnikmusic.com"+album_link_1)
-			#aCover = requests.get("http://www.sputnikmusic.com"+album_link_1)
-			#if aCover.status_code != 200:
-			#	print("Impossible de lire la page de l'album")
-
-			#https://www.sputnikmusic.com/images/albums/260807.jpg  => sans review
-			#https://www.sputnikmusic.com/images/albums/259476.jpg
-
-			# with open("""..\\SputnikMusic\\New Releases _ Sputnikmusic.html""") as fp:
-			#albumSoup = BeautifulSoup(aCover.content, 'lxml')
-			#print(albumSoup)
-			#cover = albumSoup.find_all(src=re.compile("images/albums"))
-			#print(album_1)
-			#print(album_link_1)
-			#print("covers found \n", cover)
 			imagePath_1 = "http://www.sputnikmusic.com/images/albums/"+album_link_1[7:13]+".jpg"
-			releases_list.append({'idAlbumSputnik':album_link_1[7:13],'artistName':artiste_1, 'albumName':album_1, 'note':note_release_1, 'releaseMonth':mois_lu, 'imagePath': imagePath_1})
-			# writer.writerow([artiste_1,album_1,note_release_1,album_link_1])
-			# jsondata = json.dumps(json_output)
-			# print("json data ", jsondata)
+			releaseJson = {'artistName':artiste_1, 'albumName':album_1,'sputnikMusic':{'id':album_link_1[7:13], 'note':note_release_1, 'releaseDate':{ 'month': monthDic[releaseDate[0]], 'year': releaseDate[1]}, 'imagePath': imagePath_1}}
+			releases_list.append(releaseJson)
 			j+=1
 
 
@@ -108,7 +82,6 @@ for table in table_releases:
 
 				album_link_2 = release2.next_sibling.find("a").get('href')
 
-
 				k=0
 				for string in release2.next_sibling.stripped_strings:
 				 	if k==0:
@@ -118,29 +91,10 @@ for table in table_releases:
 				 		album_2=string
 				 		k=0
 
-				# print("Artiste :", artiste_2)
-				# print ("Album :", album_2)
-				# print("Note :", note_release_2)
-				# print("Lien de l'album :", album_link_2)
-
-				#bCover = requests.get("http://www.sputnikmusic.com"+album_link_2)
-				#if bCover.status_code != 200:
-				#	print("Impossible de lire la page de l'album")
-
-			#https://www.sputnikmusic.com/images/albums/260807.jpg  => sans review
-			#https://www.sputnikmusic.com/images/albums/259476.jpg
-
-				# with open("""..\\SputnikMusic\\New Releases _ Sputnikmusic.html""") as fp:
-				#albumSoup = BeautifulSoup(bCover.content, 'lxml')
-				#print(albumSoup)
-				#cover_2 = albumSoup.find_all(src=re.compile("images/albums"))
-				#print(album_1)
-				#print(album_link_1)
-				#print("covers found \n", cover)
 				imagePath_2 = "http://www.sputnikmusic.com/images/albums/"+album_link_2[7:13]+".jpg"
-
-				# writer.writerow([artiste_2,album_2,note_release_2,album_link_2])
-				releases_list.append({'idAlbumSputnik':album_link_2[7:13],'artistName':artiste_2, 'albumName':album_2, 'note':note_release_2, 'releaseMonth':mois_lu, 'imagePath':imagePath_2})
+				releaseJson2 = {'artistName':artiste_2, 'albumName':album_2,'sputnikMusic':{'id':album_link_2[7:13], 'note':note_release_2, 'releaseDate':{ 'month': monthDic[releaseDate[0]], 'year': releaseDate[1]}, 'imagePath': imagePath_2}}
+				releases_list.append(releaseJson2)
+				#releases_list.append({'idAlbumSputnik':album_link_2[7:13],'artistName':artiste_2, 'albumName':album_2, 'note':note_release_2, 'releaseMonth':mois_lu, 'imagePath':imagePath_2})
 				j+=1
 			i+=1
 
@@ -151,9 +105,10 @@ with open('data.json', 'w', encoding='iso-8859-1') as f:
 f.close()
 
 ##### UPSERT IN DB - COLLECTION :ALBUMS
-#connection = MongoClient("mongodb://127.0.0.1:27017/playlistifyApp") mongodb://heroku_j6lv18qq:k7100p7qnmkdo5kk7i9io0q6ap@ds243418.mlab.com:43418/heroku_j6lv18qq
+connection = MongoClient("mongodb://127.0.0.1:27017/playlistifyApp")
+#mongodb://heroku_j6lv18qq:k7100p7qnmkdo5kk7i9io0q6ap@ds243418.mlab.com:43418/heroku_j6lv18qq
 #connection = MongoClient("mongodb://heroku_j6lv18qq:k7100p7qnmkdo5kk7i9io0q6ap@ds243418.mlab.com:43418/heroku_j6lv18qq?authSource=admin")
-connection = MongoClient("mongodb://webScrapper:akill007@ds243418.mlab.com:43418/heroku_j6lv18qq?authSource=heroku_j6lv18qq")
+#connection = MongoClient("mongodb://webScrapper:akill007@ds243418.mlab.com:43418/heroku_j6lv18qq?authSource=heroku_j6lv18qq")
 # mongodb://<dbuser>:<dbpassword>@ds243418.mlab.com:43418/heroku_j6lv18qq
 #connection = MongoClient("mongodb://127.0.0.1:27017/playlistifyApp")
 db = connection.get_default_database()
@@ -162,4 +117,5 @@ albums = open("data.json", "r")
 parsedAlbums = json_util.loads(albums.read())
 
 for album in parsedAlbums:
-  releases.update_one({'idAlbumSputnik': album['idAlbumSputnik'] }, {"$set": album}, upsert=True)
+	# print("PRINT ALBUM ", album["sputnikMusic"]["id"])
+	releases.update_one({'sputnikMusic.id': album['sputnikMusic']['id'] }, {"$set": album}, upsert=True)
